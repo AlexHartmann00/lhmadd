647871154be8ef4b59d7ab4094e14dac7da94f0e
AlexHartmann00 <alex@ingohartmann.de>
2022-06-17 21:36:10 +0200

    fix conflicts

--- R/robust_vcov.R
+++ R/robust_vcov.R
@@ -6,13 +6,24 @@
 #'
 #'@return Covariance matrix
 #'
-#'@export
-robust_vcov <- function(object){
-  mt <- .modelType(object)
-  if(mt == "nlme"){
-    return(.robust_vcov_nlme(object))
+#' @export
+robust_vcov <- function(object,modelmat,type="HC1"){
+  X <- modelmat
+  k <- ncol(X)
+  n <- nrow(X)
+  if(type == "HC1"){
+    hc <- (n/(n-k))*resid(object)^2
+  }
+  else if(type == "HC3"){
+    hc <- resid(m)^2/(1 - hatvalues(m))^2
   }
   else{
-    return(.robust_vcov_lm_lme4(object))
+    stop("Type must be any of {HC1, HC3}")
   }
+  sandwich <- solve(t(X) %*% X)
+  meat <- (t(X) %*% (hc*diag(n)) %*% X)
+  vce <- solve(t(X) %*% X) %*%
+    (t(X) %*% (hc*diag(n)) %*% X) %*%
+    solve(t(X) %*% X)
+  return(sandwich %*% meat %*% sandwich)
 }
